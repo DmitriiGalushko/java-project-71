89ed7ae0b55569021a7358da0e6257585bf3a94d
DmitriiGalushko <dimagalushko1988@gmail.com>
2023-06-01 21:28:43 +0300

    add option and parameters to App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,11 +3,22 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
 class App {
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format",
+            defaultValue = "stylish", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+    private String format;
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+    
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
